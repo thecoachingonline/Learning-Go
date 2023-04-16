edea6ffc8bec6408f40f5189868d3250c385f382
Y.Nanthachai <thecoachingonline@gmail.com>
2023-04-16 03:38:31 +0000

    Variable

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+var numberInt, numberInt2 int = 1000, 2000
+var msg string = "Hello "
+
 func main() {
-	fmt.Println("Hello Code Snippet")
+	numberfloat := 25.4
+	fmt.Println(numberInt)
+	fmt.Println(numberInt2)
+	fmt.Println(numberfloat)
+	fmt.Println(msg)
+
+	fmt.Println(numberInt + numberInt2)
+	fmt.Println(float64(numberInt) + numberfloat)
+	fmt.Println(msg + "World")
+	fmt.Println("My money =", numberInt)
 }
