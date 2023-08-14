49f2db293b95a7278d7ad67be0c5998c5316b0d2
Y.Nanthachai <thecoachingonline@gmail.com>
2023-08-14 02:27:57 +0000

    Forloop J

--- loop/forloop.go
+++ loop/forloop.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const count = 15
 
 func main() {
 	i := 0
 	for i < 10{
 		fmt.Println("i =", i)
 		i = i + 1
+	}
+	
+	for j := 0; j < count; j++ {
+		fmt.Println("j =", j)
 	}
 }
