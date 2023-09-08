c0d58c2ec99b4b4eb986e2e7eb525f934c3f7eab
Y.Nanthachai <thecoachingonline@gmail.com>
2023-09-08 04:46:51 +0000

    SwitchCase

---
+++ switchcase/switchcase.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	input := 2
+	switch input {
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	case 3:
+		fmt.Println("Three")
+	default:
+		fmt.Println("invalid value")
+	}
+}
