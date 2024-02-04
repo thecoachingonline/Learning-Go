03e5d7cb93c23d846206fd2a0d753e24811b142e
Y.Nanthachai <thecoachingonline@gmail.com>
2024-02-04 01:12:52 +0000

    Store ordered values in arrays

--- ComplexTypesandCollections/main.go
+++ ComplexTypesandCollections/main.go
@@ -16,4 +16,17 @@ func main() {
 	*pointer1 = *pointer1 / 31
 	fmt.Println("Pointer 1:", *pointer1)
 	fmt.Println("Value 1:", value1)
+
+	var colors[3] string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+	fmt.Println(colors)
+	fmt.Println(colors[0])
+
+	var number = [5] int{5, 3, 1, 2, 4}
+	fmt.Println(number)
+
+	fmt.Println("Number of colors:", len(colors))
+	fmt.Println("Number of number:", len(number))
 }
