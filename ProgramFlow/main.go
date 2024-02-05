ee31226c1d793824cafd5c96d7aa1c5eb35bdd5d
Y.Nanthachai <thecoachingonline@gmail.com>
2024-02-05 14:46:48 +0000

    Create loops with for statements

--- ProgramFlow/main.go
+++ ProgramFlow/main.go
@@ -45,4 +45,37 @@ func main() {
 		result1 = "It's some other day!"
 	}
 	fmt.Println(result1)
+
+	colors := []string{"Red", "Green", "Blue"}
+	fmt.Println(colors)
+
+	for i := 0; i < len(colors); i++ {
+		fmt.Println(colors[i])
+	}
+
+	for i := range colors {
+		fmt.Println(colors[i])
+	}
+
+	for _, color := range colors {
+		fmt.Println(color)
+	}
+
+	value := 1
+	for value < 10 {
+		fmt.Println("Value:", value)
+		value++
+	}
+
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+		fmt.Println("Sum:", sum)
+		if sum < 200 {
+			goto theEnd
+		}
+	}
+
+	theEnd:
+	fmt.Println("End of program")
 }
