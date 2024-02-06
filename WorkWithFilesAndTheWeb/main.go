258ec1e9b188fa1ef75d72f60f999485e1c69770
Y.Nanthachai <thecoachingonline@gmail.com>
2024-02-06 03:42:47 +0000

    Read a text file from the web

--- WorkWithFilesAndTheWeb/main.go
+++ WorkWithFilesAndTheWeb/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
-	"os"
+	"net/http"
 )
 
+const url ="https://catalogapi.nso.go.th/api/index?table=SES_OS_19&format=json"
+
 func main()  {
+/*
 	content := "สวัสดี จากภาษา Go!"
 	file, err := os.Create("./fromString.txt")
 	checkError(err)
@@ -16,8 +18,26 @@ func main()  {
 	fmt.Printf("Wrote a file with %v characters\n", length)
 	defer file.Close()
 	defer readFile("./fromString.txt")
-}
+*/
+	// อ่านข้อความไฟล์จากเว็บ
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
 
+	fmt.Printf("Response type: %T\n", resp)
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(bytes)
+	fmt.Print(content)
+}
+/*
 func readFile(fileName string)  {
 	data, err := ioutil.ReadFile(fileName)
 	checkError(err)
@@ -29,3 +49,4 @@ func checkError(err error)  {
 		panic(err)
 	}
 }
+*/
