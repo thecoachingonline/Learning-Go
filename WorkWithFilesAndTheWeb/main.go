e7a65e58938991c2189e3ca81b7f3e41be7cd255
Y.Nanthachai <thecoachingonline@gmail.com>
2024-02-06 03:57:35 +0000

    Parse JSON-formatted text

--- WorkWithFilesAndTheWeb/main.go
+++ WorkWithFilesAndTheWeb/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const url ="https://catalogapi.nso.go.th/api/index?table=SES_OS_19&format=json"
@@ -35,7 +37,12 @@ func main()  {
 	}
 
 	content := string(bytes)
-	fmt.Print(content)
+	// fmt.Print(content)
+
+	tours := toursFromJson(content)
+	for _, tour := range tours {
+		fmt.Println(tour.Name)
+	}
 }
 /*
 func readFile(fileName string)  {
@@ -50,3 +57,27 @@ func checkError(err error)  {
 	}
 }
 */
+
+func toursFromJson(content string) []Tour  {
+	tours := make([]Tour, 0, 20)
+
+	decoder := json.NewDecoder(strings.NewReader(content))
+	_, err := decoder.Token()
+	if err != nil {
+		panic(err)
+	}
+
+	var tour Tour
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		if err != nil {
+			panic(err)
+		}
+		tours = append(tours, tour)
+	}
+	return tours
+}
+
+type Tour struct {
+	Name, Price string
+}
